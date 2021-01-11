c6ded26c044d893eeea276cbe9dce86e208769dc
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-VKM0MRH>
2021-01-11 08:39:20 -0300

    Oi seu feio 2!

---
+++ noticias.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<--Testando...-->
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Notícias</title>
+
+        <!--Bootstrap-->
+        <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+
+        <!--Plugins - Icomoon-->
+        <link rel="stylesheet" href="plugins/icomoon/icomoon.css">
+
+        <!--Meu CSS-->
+        <link rel="stylesheet" href="css/noticias.css">
+
+    </head>
+    <body>
+
+        <div class="container my-5">
+            <div class="row">
+                <div class="col">
+                    <div class="row">
+                        <img src="https://placehold.it/600x450" alt="">
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col pl-0">
+                            <img class="img-fluid" src="https://placehold.it/600x450" alt="">
+                        </div>
+                        <div class="col">
+                            <img class="img-fluid" src="https://placehold.it/600x450" alt="">
+                        </div>
+                        <div class="col pr-0">
+                            <img class="img-fluid" src="https://placehold.it/600x450" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <h1>Lorem ipsum</h1>
+                    <p class="text-justify">Lorem Ipsum is simply dummy text of 
+                        the printing and typesetting industry. Lorem Ipsum has 
+                        been the industry's standard dummy text ever since the 
+                        1500s, when an unknown printer took a galley of type and 
+                        scrambled it to make a type specimen book. remaining 
+                        essentially unchanged. It was popularised in the 1960s 
+                        with the release ocimen book. It has survived not only 
+                        five centuries, but also the leap into electronic 
+                        typesetting, remaining essentially unchanged. It was 
+                        popularised in the 1960s with the release ocimen book. 
+                        It has survived not only five centuries, but also the 
+                        leap into ease ocimen book. It has survived not only 
+                        five centuries, but also the leap into electronic 
+                        typesetting, remaining essentially unchanged. It wasIt 
+                        has survived not only five centuries, but also the leap 
+                        into electronic typesetting, remaining essentially 
+                        unchanged. It was popularised in the 1960s with the 
+                        release ocimen book. It has survived not only five 
+                        centuries, but also the leap into electronic 
+                        typesetting, remaining essentially unchanged. It was 
+                        popularised in the 1960s with the release ocimen book. 
+                        It has survived not only five centuries, but also the 
+                        leap into ease ocimen book. It has survived not only 
+                        five centuries, but also the leap into electronic 
+                        typesetting, remaining essentially unchanged. It was 
+                        popularised in the 1960s with the release of Letraset 
+                        sheets containing Lorem Ipsum passages, and more 
+                        recently with desktop publishing software like Aldus 
+                        PageMaker including versions of Lorem Ipsum.
+                    </p>
+                </div>    
+            </div>
+
+            <div class="row mt-3">
+                <div class="col pl-0">
+                    <img class="img-fluid" src="https://placehold.it/600x450" alt="">
+                </div>
+                <div class="col">
+                    <img class="img-fluid" src="https://placehold.it/600x450" alt="">
+                </div>
+                <div class="col">
+                    <img class="img-fluid" src="https://placehold.it/600x450" alt="">
+                </div>
+                <div class="col">
+                    <img class="img-fluid" src="https://placehold.it/600x450" alt="">
+                </div>
+
+            </div>
+            <div class="row my-4">
+                <div class="col bordas-laterais text-center p-4">
+                    <h2>Loren Ipsum</h2>
+                    <p class="text-justify">Lorem Ipsum is simply dummy text of the printing and typesetting industryext of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the </p>
+                    <button class="btn btn-primary">
+                        <span class="reg-plus"></span>
+                        Ver mais
+                    </button>
+                </div>
+                <div class="col bordas-laterais text-center p-4 mx-3">
+                    <h2>Loren Ipsum</h2>
+                    <p class="text-justify">Lorem Ipsum is simply dummy text of the printing and typesetting industryext of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the </p>
+                    <button class="btn btn-primary">
+                        <span class="reg-plus"></span>
+                        Ver mais
+                    </button>
+                </div>               
+            </div>
+
+            <div class="row text-center">
+                <div class="col">
+                    <ul class="list-group rodape">
+                        <li class="list-group-item"><a class="arredondado btn-info btn-block  px-5 p-2" href="">Cras justo odio</a></li>
+                        <li class="list-group-item"><a class="arredondado btn-info btn-block  px-5 p-2" href="">Cras justo odio</a></li>
+                        <li class="list-group-item"><a class="arredondado btn-info btn-block  px-5 p-2" href="">Cras justo odio</a></li>
+                        <li class="list-group-item"><a class="arredondado btn-info btn-block  px-5 p-2" href="">Cras justo odio</a></li>
+                    </ul>
+                </div>
+                <div class="col py-5">
+                    <img class="img-fluid img-thumbnail arredondado"src="https://placehold.it/70x70" alt="">
+                    <h5>Lorem Ipsum</h5>
+                    <p>Lorem ipsunm Lorem ipsunm</p>
+                </div>
+                <div class="col">
+                    <ul class="list-group rodape">
+                        <li class="list-group-item"><a class="arredondado btn-info btn-block  px-5 p-2" href="">Cras justo odio</a></li>
+                        <li class="list-group-item"><a class="arredondado btn-info btn-block  px-5 p-2" href="">Cras justo odio</a></li>
+                        <li class="list-group-item"><a class="arredondado btn-info btn-block  px-5 p-2" href="">Cras justo odio</a></li>
+                        <li class="list-group-item"><a class="arredondado btn-info btn-block  px-5 p-2" href="">Cras justo odio</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        
+        <!--Fim Container-->
+
+    </body>
+</html>
+
---
+++ posts.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Posts</title>
+
+        <!--Bootstrap-->
+        <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+
+        <!--Icomoon-->
+        <link rel="stylesheet" href="plugins/icomoon/icomoon.css">
+
+        <!--Meu CSS-->
+        <link rel="stylesheet" href="css/posts.css">
+    </head>
+    <body>
+
+        <div class="container">
+            <div class="row justify-content-center mt-4">
+                <div class="col-4 text-center alert-primary p-4">
+                    <img class="img-fluid img-thumbnail arredondamento" src="https://placehold.it/250x200" alt="">
+                    <h3>Loren Ipsum Loren</h3>
+                    <hr>
+                    <p>
+                        Silvio Santos Ipsum Ma o Silvio Santos Ipsum é 
+                        muitoam interesanteam. Com ele ma você vai 
+                        gerar textuans ha haae. Ma vejam só, vejam só.
+                    </p>
+                    <button class="btn-block-outline btn-primary btn-block btn-lg arredondamento-btn"> 
+                        <span class="reg-plus"></span> Ver mais</button>
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
